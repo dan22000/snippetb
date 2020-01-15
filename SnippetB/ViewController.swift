babc31f0f0b98a10903e584b3e8c1cc6a89f2bdd
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2020-01-15 12:59:01 +0100

    Initial Commit

---
+++ SnippetB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnippetB
+//
+//  Created by Daniel Wohlmuth on 15.01.20.
+//  Copyright © 2020 Daniel Wohlmuth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
